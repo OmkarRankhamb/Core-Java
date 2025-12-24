071bdb08a5bb2620f033e2d2c9c6e46cceeb3a6d
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-24 23:38:32 +0530

    abstraction

--- oops/src/Inheritance/Fifth.java
+++ oops/src/Inheritance/Fifth.java
@@ -3,44 +3,37 @@ package Inheritance;
 public class Fifth {
 
 	public static void main(String[] args) {
-		Two_wheeler two=new Two_wheeler("Harley_Davidson");
+		Two_wheeler two = new Two_wheeler("Harley_Davidson");
 	}
 
 }
 
-class Vehicle
-{
+class Vehicle {
 	String name;
-	
-	public Vehicle(String name)
-	{
-		this.name=name;
-		System.out.println("value assigned in the super class:"+name);
+
+	public Vehicle(String name) {
+		this.name = name;
+		System.out.println("value assigned in the super class:" + name);
 	}
-	
+
 }
-class Four_wheeler extends Vehicle
-{
-	
-	public Four_wheeler(String name)
-	{
+
+class Four_wheeler extends Vehicle {
+
+	public Four_wheeler(String name) {
 		super(name);
 	}
 }
 
-class Three_wheeler extends Four_wheeler
-{
-	public Three_wheeler(String name)
-	{
+class Three_wheeler extends Four_wheeler {
+	public Three_wheeler(String name) {
 		super(name);
 	}
 
 }
 
-class Two_wheeler extends Three_wheeler
-{
-	public Two_wheeler(String name)
-	{
+class Two_wheeler extends Three_wheeler {
+	public Two_wheeler(String name) {
 		super(name.toUpperCase());
 	}
 
---
+++ oops/src/abstraction/First.java
@@ -0,0 +1,52 @@
+package abstraction;
+
+public class First {
+
+	public static void main(String[] args) {
+		Tiger tiger = new Tiger();
+
+		tiger.race();
+
+		System.out.println(tiger.whatyoueat("grass"));
+
+		tiger.drink();
+
+		Tiger siber = new Tiger();
+
+		System.out.println(siber.whatyoueat("Corniverous"));
+	}
+
+}
+
+abstract class Animal {
+
+	public Animal() {
+		System.out.println("Object.. hehe");
+	}
+
+	abstract void race();
+
+	abstract public String whatyoueat(String n);
+
+	public void drink() {
+		System.out.println("I am Drinking... water");
+	}
+}
+
+class Tiger extends Animal {
+
+	public Tiger() {
+		super();
+		System.out.println("Tiger hehe");
+	}
+
+	void race() {
+
+		System.out.println("Race implemented..");
+	}
+
+	public String whatyoueat(String n) {
+		return n;
+	}
+
+}

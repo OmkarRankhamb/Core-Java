f40bd7b490cdebe4d66584a98219ab41bd4d5f74
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-25 15:02:57 +0530

    Constructor

---
+++ Constructor/src/Default/First.java
@@ -0,0 +1,16 @@
+package Default;
+
+public class First {
+
+	public static void main(String[] args) {
+     Demo d1 = new Demo();
+     
+	}
+}
+
+class Demo {
+	Demo() {
+		System.out.println("calling to the Default Constructor");
+
+	}
+}

1689213e850232dfe22c05e60dcee881a851c582
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-25 12:32:41 +0530

    Encapsulation

---
+++ oops/src/Encapsulation/Second.java
@@ -0,0 +1,31 @@
+package Encapsulation;
+
+public class Second {
+	  public static void main(String[] args) {
+
+		  
+		    Area rectangle = new Area(5, 6);
+		    rectangle.getArea();
+		  }
+		}
+
+
+	class Area {
+
+		  int length;
+		  int breadth;
+
+		  
+		  Area(int length, int breadth) {
+		    this.length = length;
+		    this.breadth = breadth;
+		  }
+
+		  public void getArea() {
+		    int area = length * breadth;
+		    System.out.println("Area: " + area);
+		  }
+		
+	}
+	
+		

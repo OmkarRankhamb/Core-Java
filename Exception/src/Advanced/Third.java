bc39e98f1062ec7ab9fb8f006f4d46dc7bd765fc
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-28 23:42:46 +0530

    Exception

---
+++ Exception/src/Advanced/Third.java
@@ -0,0 +1,22 @@
+package Advanced;
+
+public class Third {
+
+	public static void main(String[] args) throws Exception {
+		
+		if(10>5 && 40>30)
+		{
+			throw  new Exception("Chukicha Diwas!");
+		}
+		
+		
+		System.out.println("THE END");
+	}
+
+}
+	
+	
+	
+	
+	
+	

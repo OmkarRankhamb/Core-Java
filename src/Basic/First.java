1a19d32e30d2c37c8efbdda1a89953703f146ca2
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-28 22:29:20 +0530

    first

---
+++ src/Basic/First.java
@@ -0,0 +1,25 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+
+		
+		int i;
+		
+		Scanner scan=new Scanner(System.in);
+		i=scan.nextInt();
+		
+		System.out.println(10/i);
+		
+		
+		System.out.println("hiii");
+		System.out.println("I am innocent");
+	}
+
+
+	}
+
+

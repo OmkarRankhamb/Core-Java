b5a9a5c76969f9fc95260785ec94150439b38831
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-28 23:21:04 +0530

    Exception

---
+++ Exception/src/Basics/Sixth.java
@@ -0,0 +1,25 @@
+package Basics;
+
+public class Sixth {
+
+	public static void main(String[] args) {
+
+		int age=12;
+		  try {
+		if(age<18)
+		{
+			 throw new Exception("Invalid  age");
+		}
+		System.out.println("you can vote....");
+		  }
+		  catch(Exception e)
+		  {
+			  System.out.println(e.getMessage());
+		  }
+		  finally
+		  {
+			  System.out.println("hello");
+		  }
+	}
+
+}

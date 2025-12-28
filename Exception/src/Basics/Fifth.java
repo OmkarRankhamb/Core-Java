786a60a3c44091814716972624e05ef973c87eb9
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-28 23:18:57 +0530

    Exception

---
+++ Exception/src/Basics/Fifth.java
@@ -0,0 +1,37 @@
+package Basics;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Fifth {
+
+	public static void main(String [] args) {
+		
+		
+		int str;
+		
+		Scanner scan=new Scanner(System.in);
+
+		
+		try {
+		str=scan.nextInt();
+		System.out.println(str);
+		}
+		catch(InputMismatchException e)
+		{
+			System.out.println("in the exception");
+			System.out.println(e);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		finally
+		{
+			System.out.println("---THE END---");
+		}
+		
+	}
+}
+	

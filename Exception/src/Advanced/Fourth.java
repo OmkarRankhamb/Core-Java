6a1f773e52c9d1c9c98e51d244393f5b4ffe0909
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-28 23:47:48 +0530

    Exception

---
+++ Exception/src/Advanced/Fourth.java
@@ -0,0 +1,18 @@
+package Advanced;
+
+public class Fourth {
+
+	public class invalidDayException extends RuntimeException {
+
+		public invalidDayException()
+		{
+			 super("Wrong day  !");
+		}
+		public invalidDayException(String str)
+		{
+			 super(str);
+		}
+         
+}
+	 
+}

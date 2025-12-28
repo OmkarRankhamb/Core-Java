2485d98521add16cf5cc2c770ce1296149b31df5
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-28 23:12:27 +0530

    Exception

---
+++ Exception/src/Basics/Third.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class Third {
+ public void main(String []args) {
+		
+		int i=10;
+		int j=0;
+		
+		/**
+		 * Try Block => Critical code.
+		 */
+		try {
+		int c=i/j;
+		}
+		finally
+		{
+			System.out.println('c');
+			
+			System.out.println("hello");
+
+		}
+		
+		System.out.println("hello2");
+		
+	}
+
+}

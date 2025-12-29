74c9da459251891dd44f657639b1fc0ee8643da2
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-29 23:44:06 +0530

    String

---
+++ String/src/StringBuffer/lineSeparator.java
@@ -0,0 +1,17 @@
+package StringBuffer;
+
+public class lineSeparator {
+
+	public static void main(String[] args) {
+
+//		class GFG {
+                     // String name = "fgf";
+                    		  
+                    		  
+//			{
+				String newline = System.lineSeparator();
+
+				System.out.println("GFG" + newline + "gfg");
+			}
+		
+	}

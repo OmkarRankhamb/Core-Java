8efd2e3cffd07ff8951350c87a4a9998be1da494
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-29 22:09:36 +0530

    String

---
+++ String/src/Demo/first.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class first {
+
+	public static void main(String[] args) {
+		
+		String  name =" Applicatin of Computer Scince "; 
+     char c [] = {'a','b','c','d','e','f','a','b','c','d','e','f'};
+     
+     String s1 = new String(c);
+     System.out.println(s1);
+     
+	}
+
+}

f38fff1ed04e10b7e59db7bd9d18fc66ce3e937b
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-29 23:08:21 +0530

    String

---
+++ String/src/Demo/Fifth.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class Fifth {
+
+	public static void main(String[] args) {
+            String First = "Java Programing";
+            String Second = "cpp Programing ";
+            String Third = "Java Programing";
+            
+            boolean result1 = First.equals(Second);
+            System.out.println(result1);
+            
+            boolean result2 = First.equals(Third);
+            System.out.println(result2);
+            
+            boolean result3 = Second.equals(First);
+            System.out.println(result3);
+	}
+
+}

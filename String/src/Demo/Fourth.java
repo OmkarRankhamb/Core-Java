6a87f90056c9164acce9c806a31e3ed4e485dc94
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-29 23:01:22 +0530

    String

---
+++ String/src/Demo/Fourth.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Fourth {
+
+	public static void main(String[] args) {
+         String first = "Java";
+         System.out.println("first :"+first);
+         
+         
+         String Second = "Programing";
+         System.out.println("Second: "+ Second);
+         
+         String joindString = first.concat(Second);
+         System.out.println("joindString: "+joindString);
+	}
+
+}

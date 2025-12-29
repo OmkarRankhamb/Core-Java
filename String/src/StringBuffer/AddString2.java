eefcd474f8eb07d5201ad23767307f4ff05e1166
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-29 23:51:57 +0530

    String

---
+++ String/src/StringBuffer/AddString2.java
@@ -0,0 +1,14 @@
+package StringBuffer;
+
+public class AddString2 {
+
+	public static void main(String[] args) {
+
+		char a = 'G';
+		String str = "eeksforGeeks";
+
+		String str2 = a + str;
+
+		System.out.println(str2);
+	}
+}

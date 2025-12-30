ee7283dddc2b5ee340f6394a45fb52f71bd7b953
OmkarRankhamb <omrankhamb7@gmail.com>
2025-12-30 20:19:42 +0530

    NonaccessModifiers

---
+++ NonaccessModifiers/src/One/Final.java
@@ -0,0 +1,19 @@
+package One;
+
+public class Final {
+	public static void main(String[]args) {
+		
+		 Demo obj = new  Demo(); 
+		
+		System.out.println(obj.s1);
+		//System.out.println(obj.s2);
+	}
+}
+class Demo{
+	String s1="java";
+}
+	class extended_Demo extends  Demo { 
+     String s2 = "Pythan";
+}
+
+	

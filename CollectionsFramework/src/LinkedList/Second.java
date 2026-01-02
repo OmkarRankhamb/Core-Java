839b5c89d1c6a703bec8577f2096b469150d8bcf
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-02 21:01:29 +0530

    CollectionsFramework

---
+++ CollectionsFramework/src/LinkedList/Second.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class Second {
+
+	public static void main(String[] args) {
+		LinkedList<Student> set = new LinkedList();
+		set.add(new Student(2, 343));
+		set.add(new Student(2, 334));
+
+		System.out.println(set.size());
+		System.out.println(set.equals(set));
+		System.out.println(set.hashCode());
+		System.out.println(set.contains(set));
+	}
+
+}
+class Student{
+	int id;
+	int number;
+	
+	public Student(int id , int number) {
+		this.id=id;
+		this.number=number;
+		
+	}
+	public String ToString() {
+		return this.id+" "+this.number;
+	}
+	public int Hash() {
+		return id;
+	}
+}
+

792850e316f98dd71e3b8188f0ff39ffc7835d6c
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-02 21:44:43 +0530

    CollectionsFreamework

---
+++ CollectionsFramework/src/Vector/First.java
@@ -0,0 +1,22 @@
+package Vector;
+
+import java.util.Vector;
+
+public class First {
+
+	public static void main(String[] args) {
+        Vector<Integer> v = new Vector<Integer>();
+        
+        
+        v.add(18);
+        v.add(20);
+        v.add(30);
+        v.add(40);
+        v.add(9);
+        v.add(60);
+        
+        
+        System.out.println(v);
+	}
+
+}

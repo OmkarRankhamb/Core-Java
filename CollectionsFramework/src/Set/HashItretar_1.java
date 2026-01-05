05dab122ea4baf27b28dbad067a551da2d55e41e
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-05 21:57:41 +0530

    CollectionsFrameworks

---
+++ CollectionsFramework/src/Set/HashItretar_1.java
@@ -0,0 +1,33 @@
+package Set;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashItretar_1 {
+  public static void main(String []args) {
+	  HashSet<Integer> set = new HashSet<>();
+	  
+	  set.add(56);
+	  set.add(87);
+	  set.add(12);
+	  set.add(80);
+	  set.add(32);
+	  
+	  Iterator<Integer> tr = set.iterator();
+	  while(tr.hasNext()) {
+		  
+		  System.out.println(tr.next());
+	  }
+  }
+}
+//class Loptop{
+//	int price;
+//	String name;
+//	int model;
+//	
+//	public Loptop(int price , String name , int model) {
+//		this.price=price;
+//		this.name=name;
+//		this.model=model;
+//	}
+//}

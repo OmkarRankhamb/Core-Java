3ed28756e5499345de3f2366e8cddd39a7790507
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-05 23:26:01 +0530

    CollectionsFramework

---
+++ CollectionsFramework/src/Stack/StackIterator_1.java
@@ -0,0 +1,32 @@
+package Stack;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class StackIterator_1 {
+
+	public static void main(String[] args) {
+		Stack<Integer> trac = new Stack<>();
+
+		trac.add(23);
+		trac.add(87);
+		// trac.add(null);
+		trac.add(12);
+		// trac.add(null);
+		trac.add(98);
+
+		System.out.println(trac);
+
+		Iterator<Integer> tr = trac.iterator();
+		while (tr.hasNext()) {
+			int x = tr.next();
+			if (x == 87) {
+				tr.remove();
+				System.out.println(trac);
+
+			}
+		}
+
+	}
+
+}

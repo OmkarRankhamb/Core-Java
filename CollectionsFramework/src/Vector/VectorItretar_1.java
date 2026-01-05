e01b8e6c471a0cdb9f475dc73210fdc8fa9e82ad
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-05 23:03:33 +0530

    CollectionsFramework

---
+++ CollectionsFramework/src/Vector/VectorItretar_1.java
@@ -0,0 +1,28 @@
+package Vector;
+
+import java.util.Vector;
+
+public class VectorItretar_1 {
+
+	public static void main(String[] args) {
+        Vector <Integer> vec = new Vector<>();
+        
+        vec.add(23);
+        vec.add(65);
+        vec.add(null);
+        vec.add(87);
+        vec.add(null);
+        vec.add(23);
+        
+        System.out.println(vec);
+        
+        
+        // In vector not used Iterator 
+        
+       // Iterator(Integer) pp = vec.iterator();
+        
+        
+        
+	}
+
+}

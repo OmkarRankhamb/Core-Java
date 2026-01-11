d6703c3efc63a0d835dd12919e83060e1b2a3041
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-11 21:32:45 +0530

    Function Interface

---
+++ Funcation_Interface/src/Funcation/Function_2.java
@@ -0,0 +1,22 @@
+package Funcation;
+
+import java.util.function.Function;
+
+public class Function_2 {
+
+	public static void main(String[] args) {
+        Function<Double , String > fun =(i)->{return String.valueOf(i);};
+        System.out.println(fun.apply(112.0));
+        
+        Function <String , Integer >fun1 =(i)->{ return i.length();};
+        System.out.println(fun1.apply("omkar"));
+        
+        Function<Double , Integer > fun3 = fun1.compose(fun);
+        
+        System.out.println(fun3.apply(123456.7));
+       
+          
+        
+	}
+
+}

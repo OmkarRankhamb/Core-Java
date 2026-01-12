83a78a6be9d64c921395800463d9a0113eea7d66
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-12 22:40:00 +0530

    Stream

---
+++ Stream_API/src/Lambda_1/Sum_1.java
@@ -0,0 +1,26 @@
+package Lambda_1;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Sum_1 {
+	public static void main(String[] args) {
+		List<Integer> list = List.of(12, 22, 34, 56, 324, 22, 12, 45,2323, 56);
+//	int[] sum = {0};
+//	list.stream().forEach(i-> sum[0]+=i);
+//	System.out.println(sum[0]);
+
+//		int sum = list.stream().mapToInt(Integer::intValue).sum();
+//		System.out.println(sum);
+		
+		//Predicate<Integer> pre = (p)->p% 2==0;
+		
+//	
+
+//		int sum=
+//		list.stream().mapToInt(Integer::intValue).sum();
+//		System.out.println(sum);
+		list.stream().map(i->i+2).forEach(System.out::println);
+		
+	}
+}

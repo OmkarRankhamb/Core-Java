19215d1ec942f12878455c00cfb4fa1d6d141941
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-12 22:01:21 +0530

    Steam

---
+++ Stream_API/src/Lambda_1/Sum.java
@@ -0,0 +1,20 @@
+package Lambda_1;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Sum {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = List.of(12,23,34,56,75,78,554,45,34);
+		//System.out.println(list);
+		
+		Consumer<Integer> ctr =(i)->{System.out.println(i>22);};
+	//	System.out.println(i);
+		
+		
+		list.stream().forEach(ctr);
+	}
+
+}

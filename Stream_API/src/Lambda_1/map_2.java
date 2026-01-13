6685482f6094c12b88d74e97bc7d18b4f9399063
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-13 13:51:05 +0530

    Stream

---
+++ Stream_API/src/Lambda_1/map_2.java
@@ -0,0 +1,20 @@
+package Lambda_1;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class map_2 {
+
+	public static void main(String[] args) {
+      List<String> list=List.of("Pune","Mumbai","Latur","Dharasiv");
+      
+      
+      Function<String,String> str2 =(i)->{return i.toUpperCase();};
+      Function<String,StringBuffer > str =(i)->{return new StringBuffer(i);};
+      Consumer<StringBuffer> con =(i)->{System.out.println(i);};
+      
+      list.stream().map(str2).map(str).forEach(con);
+	}
+
+}

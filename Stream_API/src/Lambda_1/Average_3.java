45f1ad8b82b5ccdfa32a208243479dffed962cb7
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-14 15:28:45 +0530

    Stream

---
+++ Stream_API/src/Lambda_1/Average_3.java
@@ -0,0 +1,10 @@
+package Lambda_1;
+
+public class Average_3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Stream_API/src/Sort/Fifth.java
@@ -0,0 +1,16 @@
+package Sort;
+
+import java.util.List;
+
+public class Fifth {
+
+	public static void main(String[] args) {
+		System.out.println(
+         List.of(12,34,32,2,78).
+       
+       stream().max((i,j)->{return Integer.compare(i, j);}));
+         
+         
+	}
+
+}

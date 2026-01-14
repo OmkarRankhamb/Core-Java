562fda85ec3e6bab47b9137268a873e5701529c5
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-14 14:45:18 +0530

    Steram

---
+++ Stream_API/src/Sort/Second.java
@@ -0,0 +1,14 @@
+package Sort;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Second {
+
+	public static void main(String[] args) {
+           List<Integer> list=List.of(38,43,54,11,22,47);
+           
+           list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+	}
+
+}

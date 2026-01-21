c68534cac92b6ed21d58b7e23f633498e1c54546
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-22 02:00:09 +0530

    Method

---
+++ Method_refarance/src/Aribritri_type/Second.java
@@ -0,0 +1,23 @@
+package Aribritri_type;
+
+import java.util.List;
+
+public class Second {
+         
+	public static void main(String[] args) {
+		List.of("asd","sda","aasdas")
+		.stream()
+		.map(String::length)
+	//	.map(Math::sqrt)
+		.map(Demi::add)
+		
+		.forEach(System.out::println);
+
+	}
+
+}
+class Demi{
+	public static int add(int i) {
+		return i*i;
+	}
+}

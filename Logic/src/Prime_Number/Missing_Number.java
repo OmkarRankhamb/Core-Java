6290b2b0527ffbfe84344287d0eece62b26b0c7d
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-22 15:24:57 +0530

    Logic

---
+++ Logic/src/Prime_Number/Missing_Number.java
@@ -0,0 +1,29 @@
+package Prime_Number;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Missing_Number {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,3,4,6,8);
+				
+		//	IntStream.rangeClosed(1, 8)
+			//.filter(i->!list.contains(i))
+			for(int i=2; i<=8; i++) {
+				
+			
+			if (! list.contains(i)){
+				System.out.println(i);
+			}
+	}
+		
+				
+				
+				
+         
+	}
+}
+
+

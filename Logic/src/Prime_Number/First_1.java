db73c82744e88a91068d81bfe63f1c83ada23854
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-23 23:45:56 +0530

    Logic

--- Logic/src/Prime_Number/First_1.java
+++ Logic/src/Prime_Number/First_1.java
@@ -3,9 +3,9 @@ package Prime_Number;
 public class First_1 {
 
 	public static void main(String[] args) {
-          for(int i=1; i<=5; i++) {
+          for(int i=1; i<=6; i++) {
         	  for(int j=1; j<=i; j++) {
-        		  System.out.print("*");
+        		  System.out.print(j);
         	  }
         	  System.out.println();
           }

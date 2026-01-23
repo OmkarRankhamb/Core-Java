e55c40e0efc446cbe437c5332a59fb4679474233
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-23 22:15:19 +0530

    Logic

---
+++ Logic/src/Prime_Number/Star_print.java
@@ -0,0 +1,15 @@
+package Prime_Number;
+
+public class Star_print {
+
+	public static void main(String[] args) {
+         for(int i=0; i<5;i++) {
+        	 for(int j=0; j<5; j++) {
+        		 System.out.print("*");
+        	 }
+        	 System.out.println();
+         }
+         
+	}
+
+}

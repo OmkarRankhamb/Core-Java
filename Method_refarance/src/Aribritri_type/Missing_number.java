2cf5ba2fac5f3005cbdde8baca7c6ae447e90988
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-25 22:42:49 +0530

    Method refarnce

---
+++ Method_refarance/src/Aribritri_type/Missing_number.java
@@ -0,0 +1,16 @@
+package Aribritri_type;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Missing_number {
+
+	public static void main(String[] args) {
+		List<Integer> list=List.of(1,3,4,5,8,9,10);
+		
+		IntStream.rangeClosed(1, 10).peek(System.out::println)
+		.forEach(System.out::println);
+
+	}
+
+}
--- Method_refarance/src/Aribritri_type/Salary_AVG.java
+++ Method_refarance/src/Aribritri_type/Salary_AVG.java
@@ -1,7 +1,10 @@
 package Aribritri_type;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import Lambda_1.Employee;
 
 public class Salary_AVG {
 
@@ -13,7 +16,12 @@ public class Salary_AVG {
         		new Demo_4(3,"Shamo",35452.0,"IT"),
         		new Demo_4(4,"Revo",645636.0,"IT"));
         
-        list.stream().
+    Map <String,Double> output=    list.stream()
+    		.collect(Collectors.   
+        		groupingBy(Demo_4::getDepartment,
+        		Collectors.averagingDouble(Demo_4::getSalary)
+    
+   
         		
 	}
 

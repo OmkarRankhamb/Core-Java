c33bcf39cfbfb7559812f998c088047c0abf5608
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-26 23:47:24 +0530

    If_else

---
+++ If_else/src/uss/Discount_2.java
@@ -0,0 +1,26 @@
+package uss;
+
+
+
+public class Discount_2 {
+
+	public static void main(String[] args) {
+		boolean isFirstOrder=true;
+		double OrderAmount=500;
+        
+		if(OrderAmount>=400) {
+			if(isFirstOrder==true){
+				double Discount = OrderAmount*0.30;
+				double FinalAmount = OrderAmount-Discount;
+				System.out.println("you will get 30 % disconut in final Payment=>"+FinalAmount);
+			}
+		}
+		else {
+			double Disconut = OrderAmount *0.10;
+			double FinalAmount = OrderAmount-Disconut;
+			System.out.println("you will get 10 % discount in final payment => "+Disconut);
+		}
+
+	}
+
+}

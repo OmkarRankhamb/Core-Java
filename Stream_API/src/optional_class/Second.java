45b095f3944647b7d94b792e180a26a5f245480d
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-28 13:06:29 +0530

    Optional class

---
+++ Stream_API/src/optional_class/Second.java
@@ -0,0 +1,19 @@
+package optional_class;
+
+import java.util.Optional;
+
+public class Second {
+
+	public static void main(String[] args) {
+		  Optional<String> ops = Optional.ofNullable(null);
+		  
+		 // System.out.println(ops.get());
+		  
+          // System.out.println(ops.orElse("done"));
+      //     System.out.println(ops.orElseThrow(()->new RuntimeException ("value not prasent")));
+		//  System.out.println(ops.isPresent());
+           ops.ifPresentOrElse((i)->System.out.println(i),()->System.out.println("done"));
+		  
+	}
+
+}

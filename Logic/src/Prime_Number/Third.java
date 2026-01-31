18d771020d8022827831f9dc1ab0615024a0f363
OmkarRankhamb <omrankhamb7@gmail.com>
2026-01-31 22:43:59 +0530

    Logic

---
+++ Logic/src/Prime_Number/Third.java
@@ -0,0 +1,16 @@
+package Prime_Number;
+
+public class Third {
+	
+	public static void change(int x) {
+		x=100;
+	}
+
+	public static void main(String[] args) {
+		int a = 50;
+		change(a);
+		System.out.println(a);
+
+	}
+
+}

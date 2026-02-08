b2ab99e4deae738ebc9dba28b908b045424d87ee
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-08 19:10:32 +0530

    Logic

--- Logic/src/Prime_Number/For_loop.java
+++ Logic/src/Prime_Number/For_loop.java
@@ -1,11 +1,16 @@
 package Prime_Number;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class For_loop {
 
 	public static void main(String[] args) {
-	    
+	  //  sum = 9=> print tow element
 		int sum = 9;
 		int  arr[]= {1,2,3,4,5,6,7,8,9};
 		
@@ -38,25 +43,75 @@ public class For_loop {
 		
 		
 		System.out.println("===============================================");
-       
+       // prime number
+		
 		int arr2 [] = {1,2,3,4,5,6,7,8,9};
 		
 		for(int i =1; i<arr2.length;i++) {
-			int num = arr2[i];
-			
-			if(num<=1)continue;
+	
 			boolean isPrime = true;
-			for(int j =2; j<=num/2; j++) {
-			if(num  % j==0) {
+			for(int j =2; j<=i/2; j++) {
+			if(i  % j==0) {
 				isPrime=false;
 				break;
 			}
 			}
 			if(isPrime) {
-				System.out.println("prime::"+num);
+				System.out.println("prime::"+i);
 			}
 			
 			}
-
+      System.out.println("========================================================");
+      
+     // prime number
+      
+//      int n;
+//      int count =0;
+//      
+//      Scanner sc = new Scanner(System.in);
+//      System.out.println("Enter number");
+//     n= sc.nextInt();
+//      
+//      for(int i=1; i<=n; i++) {
+//    	   if(n%i==0) {
+//    		   count++;
+//    	   }
+//      }
+//     if(count ==2) {
+//    	 System.out.println("is a prime number::"+n);
+//     }
+//     else
+//     {
+//    	 System.out.println("not prime number::"+n);
+//     }
+      
+     System.out.println("===========================================");
+      
+     // max number
+     
+    List<Integer> list = List.of(12,34,56,78,34,56,67);
+ // Optional<Integer>max=  list .stream()
+	int max=	  Collections.max(list);
+		//  .reduce((i,j)->Integer.max(j,i));
+  System.out.println("Max number::"+max);
+      
+      System.out.println("============================================");
+      
+ List<Integer> list1=     List.of(12,34,54,56,12,43);
+  Optional<Integer>min =   list1 .stream().reduce((i,j)->Integer.min(j, i));
+      System.out.println("min number::"+min.get());
+      
+      System.out.println("==========================================");
+      
+    List<Integer> list2= List.of(12,34,54,56,43,12,34);
+   Map<Object, Long> map=  list2 .stream().collect(Collectors.groupingBy(i->i,Collectors.counting()));
+      
+      System.out.println(map);
+      
+      System.out.println("==================================================");
+      
+      List<Integer> list3 =List.of(12,34,45,12,34);
+      Map<Integer,Integer> mapp = list3.stream().collect(Collectors.toMap(n->n,n->1,(first,second)-> first+second));
+      System.out.println(mapp);
 }
 }

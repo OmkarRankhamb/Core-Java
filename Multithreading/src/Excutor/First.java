81acd2d384290539a0d00a5b06261c611967463a
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-10 13:27:54 +0530

    Excutor_Framwork

---
+++ Multithreading/src/Excutor/First.java
@@ -0,0 +1,46 @@
+package Excutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		
+		Runnable r1 =()->{
+			System.out.println("First");
+			System.out.println(Thread.currentThread().getName()+"working");
+		};
+		
+		Runnable r2 =()->{
+			System.out.println("second");
+			System.out.println(Thread.currentThread().getName()+"working");
+		};
+		
+		Runnable r3 = ()->{
+			System.out.println("Third");
+			System.out.println(Thread.currentThread().getName()+"Third");
+		};
+		
+		Runnable r4 = ()->{
+			System.out.println("Fourth");
+			System.out.println(Thread.currentThread().getName()+"working");
+		};
+		try {
+		
+		ExecutorService ex = Executors.newFixedThreadPool(4);
+		
+		ex.execute(r1);
+		ex.execute(r2);
+		ex.execute(r3);
+		ex.execute(r4);
+		ex.close();
+		}
+		catch(Exception rx){
+			System.out.println(rx.getMessage());
+			
+		}
+	}
+
+}

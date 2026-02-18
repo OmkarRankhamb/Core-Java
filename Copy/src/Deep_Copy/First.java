a0f040ab8f4e255e898b2769e47e46e0b7c3a966
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-18 14:42:13 +0530

    Copy

---
+++ Copy/src/Deep_Copy/First.java
@@ -0,0 +1,28 @@
+package Deep_Copy;
+
+public class First {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		Student st = new Student("Abc");
+		Student s = (Student) st.clone();
+
+		System.out.println(st.name);
+		String ab = "cdb";
+		String bc =ab;
+		System.out.println(ab);
+	}
+
+}
+
+class Student implements Cloneable {
+	String name;
+
+	Student(String name) {
+		this.name = name;
+		System.out.println("Object created......");
+	}
+
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}
---
+++ Multithreading/src/Excutor/Fifth_1.java
@@ -0,0 +1,10 @@
+package Excutor;
+
+public class Fifth_1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

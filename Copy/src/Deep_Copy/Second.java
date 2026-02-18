d3113c1d938875aedbb452fb83311085e0a9066a
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-18 14:56:31 +0530

    Deep copy

---
+++ Copy/src/Deep_Copy/Second.java
@@ -0,0 +1,25 @@
+package Deep_Copy;
+
+public class Second {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+       Emp e = new Emp();
+       e.i=45;
+       System.out.println(e.i);
+       
+       Emp p =(Emp)e.clone();
+       p.i=46;
+       System.out.println(p.i);
+	}
+
+}
+class Emp implements Cloneable{
+	int i;
+	
+	public Emp() {
+		System.out.println("Object Create.........");
+	}
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+}

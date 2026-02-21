59e7b3e2c1ec896106eadd7f6954b0689d8677a9
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-21 23:30:59 +0530

    Logic

---
+++ Logic/src/Practice/First_1.java
@@ -0,0 +1,20 @@
+package Practice;
+
+public class First_1 {
+
+	public static void main(String[] args) {
+
+		   //Factorial
+	     int num =5;
+	     int Factorial = 1;
+	     
+	     for(int i =1; i<=num; i++) {
+	    	 
+	    	      Factorial*=i;
+	     }
+	     System.out.println("Factorial::"+Factorial);
+	      
+		}
+	}
+
+

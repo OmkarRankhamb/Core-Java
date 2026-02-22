6329de80e5c77e5be876bd80d29e696762687518
OmkarRankhamb <omrankhamb7@gmail.com>
2026-02-22 15:39:04 +0530

    ststic

---
+++ AccessModifiers/src/Basic/Fourth.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class Fourth {
+
+	
+     static {
+    	  System.out.println("hello print ");
+         // System.exit(0);
+       	 }
+   		
+     static {
+    		
+ 	}
+   	 public static void main(String [] args) {
+   int x =5;
+   int y = 7;
+   
+   System.out.println(-x);
+	
+}
+    	 }
+

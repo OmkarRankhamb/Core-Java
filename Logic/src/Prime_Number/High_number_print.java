305c1b19ca742e2a8f4289ff7258a28c8f135918
OmkarRankhamb <omrankhamb7@gmail.com>
2026-03-20 19:08:15 +0530

    High number print

---
+++ Logic/src/Prime_Number/High_number_print.java
@@ -0,0 +1,14 @@
+package Prime_Number;
+
+import java.util.List;
+import java.util.Optional;
+
+public class High_number_print {
+
+	public static void main(String[] args) {
+		List<Integer> list=	List.of(23,45,66,78,87);
+	    Optional<Integer> max= list.stream().reduce((i,j)->Integer.max(i, j));
+			System.out.println(max.get());
+	}
+
+}

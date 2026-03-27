3c3c5c97297698a70eef2aa55b940308b05ec759
OmkarRankhamb <omrankhamb7@gmail.com>
2026-03-27 23:54:03 +0530

    unique element

--- Logic/src/Prime_Number/Unique_2.java
+++ Logic/src/Prime_Number/Unique_2.java
@@ -3,8 +3,22 @@ package Prime_Number;
 public class Unique_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 String sc = "aacbbdgejrioo";
+		 StringBuffer str = new StringBuffer(sc);
+		// int i;
+		 int j;
+		 for(int i =0; i<str.length();i++) {
+			 for(j=i+1; j<str.length();j++) {
+				 
+				 if(str.charAt(i)==str.charAt(j)){
+					 str.deleteCharAt(j);
+					 str.deleteCharAt(i);
+					 
+				 }
+				 
+			 }
+		 }
+		 System.out.println(str);
 	}
 
 }
